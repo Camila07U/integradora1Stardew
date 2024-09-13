780869297f28ea38f0c72cc7f97e1c711fc8c287
Camila07U <camila.u.rifo@gmail.com>
2024-09-13 01:56:50 -0500

    CropUpdates

--- src/main/java/model/Crop.java
+++ src/main/java/model/Crop.java
@@ -1,4 +1,18 @@
 package model;
 
+import java.util.Calendar;
+
 public class Crop {
+
+    private String name;
+    private Season station;
+
+    public Crop(String name, Season station) {
+
+        this.name = name;
+        this.station = station;
+
+    }
+
+
 }
