507a01c449b0f2aae6aa2221f94d4997b40a6a84
Camila07U <camila.u.rifo@gmail.com>
2024-09-13 10:31:06 -0500

    metodos de la clase Crop

--- src/main/java/model/Crop.java
+++ src/main/java/model/Crop.java
@@ -1,11 +1,13 @@
 package model;
 
-import java.util.Calendar;
-
 public class Crop {
 
     private String name;
     private Season station;
+    private int days;
+    private int min=15;
+    private int max=27;
+    private boolean rotten;
 
     public Crop(String name, Season station) {
 
@@ -14,5 +16,32 @@ public class Crop {
 
     }
 
+    public void grow() {
+        if(!rotten){
+            days++;
+            if(days > max){
+                rotten = true;
+            }
+        }
+    }
 
+    public boolean isReady(){
+        return days >= min && days <= max;
+    }
+
+    public boolean isRotten() {
+        return rotten;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public Season getStation() {
+        return station;
+    }
+
+    public int getDays() {
+        return days;
+    }
 }
