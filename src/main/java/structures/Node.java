5a5e8cb0b4dbedbfb73430363b50d525363c2968
Killiam-Toshiro <sebatianromeroleon@gmail.com>
2024-09-13 01:29:28 -0500

    Creacion de lista enlazada genericas

--- src/main/java/structures/Node.java
+++ src/main/java/structures/Node.java
@@ -1,4 +1,44 @@
 package structures;
 
-public class Node {
+
+public class Node<E> {
+    E data;
+    Node<E> next;
+
+    public Node(E data, Node<E> next)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(Crop crop) {
+        this.data = (E) crop;
+        this.next = null;
+    }
+
+    public Node(Chest chest)
+    {
+        this.data = (E) chest;
+        this.next = null;
+    }
+
+    public E getData()
+    {
+        return data;
+    }
+
+    public void setData(E data)
+    {
+        this.data = data;
+    }
+
+    public Node<E> getNext()
+    {
+        return next;
+    }
+
+    public void setNext(Node<E> next)
+    {
+        this.next = next;
+    }
 }
